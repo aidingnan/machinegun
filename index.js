bfd601ad71587d83f4cc1fe9ea619e5542d52a35
matianfu <matianfu@gmail.com>
2019-08-12 20:15:58 +0800

    wip

--- index.js
+++ index.js
@@ -4,7 +4,44 @@ const child = require('child_process')
 const crypto = require('crypto')
 const http = require('http')
 const os = require('os')
-const readline = require('readline')
+
+const blessed = require('blessed')
+const screen = blessed.screen({ fullUnicode: true })
+
+const header = ['DEVICE', '#STRESS', '#EMMC', 'READ', 'SPEED', 'WRITE', 'SPEED', '#SATA', 'READ', 'SPEED', 'WRITE', 'SPEED', '#WIFI', 'DOWNLOAD', 'SPEED']
+
+const layout = blessed.layout({
+  parent: screen,
+  width: '100%',
+  height: '100%' 
+})
+
+const blist = blessed.ListTable({
+  parent: layout,
+  width: '100%',
+  height: '50%',
+  data: [ header ]
+})
+
+const blog = blessed.log({ parent: layout,
+  keys: true,
+  width: '100%',
+  height: '50%',
+  focused: true,
+})
+
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  process.exit(0);
+})
+
+/**
+screen.on('resize', () => {
+  blist.emit('attach')
+  blog.emit('attach')
+})
+*/
+
+screen.render();
 
 // stress-ng  continous
 // mbw        repeating
@@ -16,7 +53,38 @@ const readline = require('readline')
 
 // format rate: 'NNN.N MB/s' length 10
 
+/**
+process.stdout.write('\033[2J\033[;H')
+console.log('设备              | 压力测试 | EMMC测试                                             | SATA测试                                             | WiFi测试') 
+let hs = []
+hs.push(' '.padEnd(17, ' '))
+hs.push('状态'.padEnd(6, ' '))
+hs.push('状态')
+hs.push('写入次数'.padStart(8 - 4, ' '))
+hs.push('速度'.padStart(10 - 2, ' '))
+hs.push('读取次数'.padStart(8 - 4, ' '))
+hs.push('速度'.padStart(10 - 2, ' '))
+hs.push('状态')
+hs.push('写入次数'.padStart(8 - 4, ' '))
+hs.push('速度'.padStart(10 - 2, ' '))
+hs.push('读取次数'.padStart(8 - 4, ' '))
+hs.push('速度'.padStart(10 - 2, ' '))
+hs.push('状态')
+hs.push('下载次数'.padStart(8 - 4, ' '))
+hs.push('速度'.padStart(10 - 2, ' '))
+console.log(hs.join(' | '))
+console.log(''.padEnd(169, '-'))
+
+*/
+
+// blist.setRows([ header ])
+// screen.render()
+
 const strip = string => string.replace(/[\u001B\u009B][[\]()#;?]*(?:(?:(?:[a-zA-Z\d]*(?:;[-a-zA-Z\d\/#&.:=?%@~_]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PR-TZcf-ntqry=><~]))/g, '')
+
+const _log = fs.createWriteStream('log', { flags: 'a'})
+const logWrite = string => _log.write(string)
+const log = string => _log.write(string + '\n')
 
 const Soldiers = new Map()
 
@@ -26,11 +94,7 @@ class StressNg extends EventEmitter {
     this.address = address
     this.child = child.spawn('ssh', ['-o', 'StrictHostKeyChecking no', '-tt', `root@${this.address}`, 'stress-ng', '--cpu', '4', '--io', '4', '--vm-bytes', '128M'])
 
-    this.child.stdout.once('data', data => {
-      // console.log(data.toString().trim())
-      this.emit('tick')
-    })
-
+    this.child.stdout.once('data', data => this.emit('tick'))
     this.child.on('error', err => {
       this.child.removeAllListeners()
       this.child.on('error', err => {})
@@ -69,7 +133,7 @@ class WifiTest extends EventEmitter {
     this.results = []
     this.child = child.exec(`ssh  -o "StrictHostKeyChecking no" -tt root@${this.address} nmcli d wifi connect ${ssid} password ${password}`, (err, stdout) => {
       if (err) {
-        console.log(err)
+        // console.log(err)
         this.child = null
         this.error = err
       } else {
@@ -83,7 +147,7 @@ class WifiTest extends EventEmitter {
               if (err) {
                 this.child = null
                 this.error = err
-                console.log(err)
+                // console.log(err)
                 process.exit(1)
               } else {
                 // TODO check sha256
@@ -290,6 +354,8 @@ class Soldier extends EventEmitter {
 let duration = 10
 let testBluetooth = true
 let ssid, password
+let hostAddress
+let digest
 
 fs.readFile('config.json', (err, json) => {
   if (err) throw err
@@ -306,16 +372,15 @@ fs.readFile('config.json', (err, json) => {
   password = config.wifi && config.wifi.password
 
   if (!ssid || !password) throw new Error('配置文件未提供可用的wifi配置')
-  console.log('本次测试使用如下配置：')
-  console.log(`  测试时间：${duration}分钟 （该设置尚未生效）`)
-  console.log(`  ${testBluetooth ? '' : '不'}测试蓝牙 （代码尚未支持）`)
-  console.log(`  wifi：${ssid}`)
-  console.log(`  wifi密码：${password}`)
+  blog.log('本次测试使用如下配置：')
+  blog.log(`  测试时间：${duration}分钟 （该设置尚未生效）`)
+  blog.log(`  ${testBluetooth ? '' : '不'}测试蓝牙 （代码尚未支持）`)
+  blog.log(`  wifi：${ssid}`)
+  blog.log(`  wifi密码：${password}`)
 
-  let digest
   const random = next => fs.lstat('random', (err, stats) => {
     if (err && err.code === 'ENOENT') {
-      console.log('生成测试文件用于wifi文件传输测试')
+      blog.log('生成测试文件用于wifi文件传输测试')
       crypto.randomFill(Buffer.alloc(1024 * 1024), (err, rbuf) => {
         let hash = crypto.createHash('sha256')
         let ws = fs.createWriteStream('random')
@@ -331,7 +396,7 @@ fs.readFile('config.json', (err, json) => {
       throw err
     } else {
       if (!stats.isFile() || stats.size !== 1024 * 1024 * 1024) {
-        console.log('已有测试文件类型或大小错误，重建测试文件') 
+        blog.log('已有测试文件类型或大小错误，重建测试文件') 
         fs.unlink('random', err => {
           if (err) {
             throw err
@@ -340,7 +405,7 @@ fs.readFile('config.json', (err, json) => {
           }
         })
       } else {
-        console.log('计算测试文件哈希值')
+        blog.log('计算测试文件哈希值')
         let rs = fs.createReadStream('random')
         let hash = crypto.createHash('sha256')
         rs.on('data', data => hash.update(data))
@@ -358,20 +423,12 @@ fs.readFile('config.json', (err, json) => {
       res.writeHead(200, { 'Content-Type': 'application/octet-stream' })
       rs.pipe(res)
     })
-    server.listen(3000, () => console.log('本机（作为文件下载服务器）开始侦听3000端口'))
+    server.listen(3000, () => {
+      blog.log('本机（作为文件下载服务器）开始侦听3000端口')
+      blog.log('测试开始')
+    })
 
-    loop()
-    setInterval(loop, 10 * 1000)
-    function loop () {
-/**
-      let ifaces = os.networkInterfaces()
-      let keys = Object.keys(ifaces).filter(k => k.startsWith('enx'))
-
-      ifaces = keys
-        .map(k => (ifaces[k].forEach(x => x.name = k), ifaces[k]))
-        .filter(x => x.fin
-        .map(x => x.find(k => k.family === 'IPv4'))
-*/
+    const loop = () => {
       let ifaceObj = os.networkInterfaces()
       let ifaces = []
       for (var key in ifaceObj) {
@@ -387,8 +444,8 @@ fs.readFile('config.json', (err, json) => {
         if (!Soldiers.has(x.mac)){
           let s = new Soldier(x, ssid, password)
           s.on('tick', () => {
-            process.stdout.write('\033[2J\033[;H')
-            console.log('设备              | 压力测试 | EMMC测试                                             | SATA测试                                             | WiFi测试') 
+            // process.stdout.write('\033[2J\033[;H')
+            // console.log('设备              | 压力测试 | EMMC测试                                             | SATA测试                                             | WiFi测试') 
             let hs = []
             hs.push(' '.padEnd(17, ' '))
             hs.push('状态'.padEnd(6, ' '))
@@ -405,8 +462,9 @@ fs.readFile('config.json', (err, json) => {
             hs.push('状态')
             hs.push('下载次数'.padStart(8 - 4, ' '))
             hs.push('速度'.padStart(10 - 2, ' '))
-            console.log(hs.join(' | '))
-            console.log('-------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+            // console.log(hs.join(' | '))
+            // console.log(''.padEnd(169, '-'))
+/*
             Array.from(Soldiers).forEach(kv => {
               let bs = kv[1].brief() 
               let ds = []
@@ -425,19 +483,18 @@ fs.readFile('config.json', (err, json) => {
               ds.push(bs[4].s)
               ds.push(bs[4].dTime.padStart(8, ' '))
               ds.push(bs[4].dRate.padStart(10, ' '))
-              
-              console.log(ds.join(' | '))
-              // console.log(bs)
+              // console.log(ds.join(' | '))
             })
+*/
+
           })
         }
       })
     }
 
-    process.on('SIGINT', () => {
-      console.log('')
-      console.log('Lord, I\'m fucked up. See you.')
-      process.exit(0)
-    })
+    loop()
+    setInterval(loop, 3000)
   })
 })
+
+
