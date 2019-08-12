94f6d4c5cf44c9485fd188cf50923069a1b6276e
matianfu <matianfu@gmail.com>
2019-08-12 21:16:40 +0800

    wip

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const http = require('http')
 const os = require('os')
 
 const blessed = require('blessed')
-const screen = blessed.screen({ fullUnicode: true })
 
-const header = ['DEVICE', '#STRESS', '#EMMC', 'READ', 'SPEED', 'WRITE', 'SPEED', '#SATA', 'READ', 'SPEED', 'WRITE', 'SPEED', '#WIFI', 'DOWNLOAD', 'SPEED']
+const ifacemon = require('./lib/iface-monitor')
+
+const screen = blessed.screen({ fullUnicode: true })
 
 const layout = blessed.layout({
   parent: screen,
@@ -20,7 +21,6 @@ const blist = blessed.ListTable({
   parent: layout,
   width: '100%',
   height: '50%',
-  data: [ header ]
 })
 
 const blog = blessed.log({ parent: layout,
@@ -43,47 +43,11 @@ screen.on('resize', () => {
 
 screen.render();
 
-// stress-ng  continous
-// mbw        repeating
-// download   repeating
-// dd emmc    repeating
-// dd sata    repeating
-// bluetooth  oneshot
-// led        not applicable
-
 // format rate: 'NNN.N MB/s' length 10
-
-/**
-process.stdout.write('\033[2J\033[;H')
-console.log('设备              | 压力测试 | EMMC测试                                             | SATA测试                                             | WiFi测试') 
-let hs = []
-hs.push(' '.padEnd(17, ' '))
-hs.push('状态'.padEnd(6, ' '))
-hs.push('状态')
-hs.push('写入次数'.padStart(8 - 4, ' '))
-hs.push('速度'.padStart(10 - 2, ' '))
-hs.push('读取次数'.padStart(8 - 4, ' '))
-hs.push('速度'.padStart(10 - 2, ' '))
-hs.push('状态')
-hs.push('写入次数'.padStart(8 - 4, ' '))
-hs.push('速度'.padStart(10 - 2, ' '))
-hs.push('读取次数'.padStart(8 - 4, ' '))
-hs.push('速度'.padStart(10 - 2, ' '))
-hs.push('状态')
-hs.push('下载次数'.padStart(8 - 4, ' '))
-hs.push('速度'.padStart(10 - 2, ' '))
-console.log(hs.join(' | '))
-console.log(''.padEnd(169, '-'))
-
-*/
-
-// blist.setRows([ header ])
-// screen.render()
 
 const strip = string => string.replace(/[\u001B\u009B][[\]()#;?]*(?:(?:(?:[a-zA-Z\d]*(?:;[-a-zA-Z\d\/#&.:=?%@~_]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PR-TZcf-ntqry=><~]))/g, '')
 
 const _log = fs.createWriteStream('log', { flags: 'a'})
-const logWrite = string => _log.write(string)
 const log = string => _log.write(string + '\n')
 
 const Soldiers = new Map()
@@ -93,8 +57,11 @@ class StressNg extends EventEmitter {
     super()
     this.address = address
     this.child = child.spawn('ssh', ['-o', 'StrictHostKeyChecking no', '-tt', `root@${this.address}`, 'stress-ng', '--cpu', '4', '--io', '4', '--vm-bytes', '128M'])
+    this.child.stdout.once('data', data => {
+      blog.log(`${this.address}`, '开始压力测试')
+      this.emit('tick')
+    })
 
-    this.child.stdout.once('data', data => this.emit('tick'))
     this.child.on('error', err => {
       this.child.removeAllListeners()
       this.child.on('error', err => {})
@@ -102,7 +69,6 @@ class StressNg extends EventEmitter {
       this.error = err
       this.child = null
     })
-
     this.child.on('close', (code, signal) => {
       this.child.removeAllListeners()
       let err = new Error('unexpected close')
@@ -171,21 +137,10 @@ class WifiTest extends EventEmitter {
   }
 
   brief () {
-    let s = this.child 
-      ? '运行'
-      : this.error
-        ? '错误'
-        : '停止'
-
-    let avg = this.results.length
-      ? this.results
-          .map(r => parseInt(r.trim().split(' ')[0]))
-          .reduce((sum, n) => sum + n, 0) / this.results.length
-      : 0
-
+    let s = this.child ? '运行' : this.error ? '错误' : '停止'
+    let avg = this.results.length ? this.results .map(r => parseInt(r.trim().split(' ')[0])) .reduce((sum, n) => sum + n, 0) / this.results.length : 0
     let rate = `${avg.toFixed(1).toString()} MB/s`
     let time = this.results.length.toString()
-
     return { s, dRate: rate, dTime: time }
   }
 }
@@ -199,6 +154,7 @@ class EmmcTest extends EventEmitter {
     let icmd = `ssh -o "StrictHostKeyChecking no" -tt root@${this.address} dd if=/root/test of=/dev/null bs=4M count=256 iflag=direct`
     const run = () => {
       let cmd = this.results.length > 1 ? icmd : ocmd
+      blog.log(`${this.address}`, `第${this.results.length + 1}次读写测试`)
       this.child = child.exec(cmd, (err, stdout) => {
         if (err) {
           this.child = null
@@ -222,19 +178,10 @@ class EmmcTest extends EventEmitter {
 
   brief () {
     let s = this.child ? '运行' : this.error ? '错误' : '停止'
-
     let ws = this.results.slice(0, 2)
     let rs = this.results.slice(2)
-
-    let wAvg = ws.length
-      ? ws.map(r => parseInt(r.trim().split(' ')[0])) 
-          .reduce((sum, n) => sum + n, 0) / ws.length
-      : 0
-    let rAvg = rs.length
-      ? rs.map(r => parseInt(r.trim().split(' ')[0])) 
-          .reduce((sum, n) => sum + n, 0) / rs.length
-      : 0
-
+    let wAvg = ws.length ? ws.map(r => parseInt(r.trim().split(' ')[0])) .reduce((sum, n) => sum + n, 0) / ws.length : 0
+    let rAvg = rs.length ? rs.map(r => parseInt(r.trim().split(' ')[0])) .reduce((sum, n) => sum + n, 0) / rs.length : 0
     let wRate = `${wAvg.toFixed(1).toString()} MB/s`
     let wTime = ws.length.toString()
     let rRate = `${rAvg.toFixed(1).toString()} MB/s`
@@ -285,29 +232,15 @@ class SataTest extends EventEmitter {
   }
 
   brief () {
-    let s = this.child 
-      ? '运行'
-      : this.error
-        ? '错误'
-        : '停止'
-
+    let s = this.child ? '运行' : this.error ? '错误' : '停止'
     let ws = this.results.slice(0, 2)
     let rs = this.results.slice(2)
-
-    let wAvg = ws.length
-      ? ws.map(r => parseInt(r.trim().split(' ')[0])) 
-          .reduce((sum, n) => sum + n, 0) / ws.length
-      : 0
-    let rAvg = rs.length
-      ? rs.map(r => parseInt(r.trim().split(' ')[0])) 
-          .reduce((sum, n) => sum + n, 0) / rs.length
-      : 0
-
+    let wAvg = ws.length ? ws.map(r => parseInt(r.trim().split(' ')[0])) .reduce((sum, n) => sum + n, 0) / ws.length : 0
+    let rAvg = rs.length ? rs.map(r => parseInt(r.trim().split(' ')[0])) .reduce((sum, n) => sum + n, 0) / rs.length : 0
     let wRate = `${wAvg.toFixed(1).toString()} MB/s`
     let wTime = ws.length.toString()
     let rRate = `${rAvg.toFixed(1).toString()} MB/s`
     let rTime = rs.length.toString()
-   
     return { s, wTime, wRate, rTime, rRate }
   }
 }
@@ -315,13 +248,12 @@ class SataTest extends EventEmitter {
 class Soldier extends EventEmitter {
   constructor(iface, ssid, password) {
     super()
-    this.iface = iface
+    this.name = iface.name
+    this.mac = iface.mac
+    this.address = iface.buddyIp
     this.ssid = ssid
     this.password = password
     this.name = iface.name
-    this.mac = iface.mac
-    this.address = '169.254.'+ this.mac.split(':').slice(3,5).map(x => parseInt(x)).join('.')
-    Soldiers.set(this.mac, this)
     this.run()
   }
   
@@ -425,76 +357,68 @@ fs.readFile('config.json', (err, json) => {
     })
     server.listen(3000, () => {
       blog.log('本机（作为文件下载服务器）开始侦听3000端口')
-      blog.log('测试开始')
+    })
+  })
+})
+
+ifacemon.on('add', iface => {
+  let soldier = new Soldier(iface, ssid, password)   
+  soldier.on('tick', () => {
+    // process.stdout.write('\033[2J\033[;H')
+    // console.log('设备              | 压力测试 | EMMC测试                                             | SATA测试                                             | WiFi测试') 
+    let data = []
+    let hs = []
+    hs.push('设备地址'.padEnd(9, ' '))
+    hs.push('状态'.padEnd(6, ' '))
+    hs.push('状态')
+    hs.push('写入次数'.padStart(8 - 4, ' '))
+    hs.push('速度'.padStart(10 - 2, ' '))
+    hs.push('读取次数'.padStart(8 - 4, ' '))
+    hs.push('速度'.padStart(10 - 2, ' '))
+    hs.push('状态')
+    hs.push('写入次数'.padStart(8 - 4, ' '))
+    hs.push('速度'.padStart(10 - 2, ' '))
+    hs.push('读取次数'.padStart(8 - 4, ' '))
+    hs.push('速度'.padStart(10 - 2, ' '))
+    hs.push('状态')
+    hs.push('下载次数'.padStart(8 - 4, ' '))
+    hs.push('速度'.padStart(10 - 2, ' '))
+    data.push(hs)
+
+    Array.from(Soldiers).forEach(kv => {
+      let bs = kv[1].brief() 
+      let ds = []
+      ds.push(bs[0].padEnd(17, ' '))
+      ds.push(bs[1].s.padEnd(6, ' '))
+      ds.push(bs[2].s)
+      ds.push(bs[2].wTime.padStart(8, ' '))
+      ds.push(bs[2].wRate.padStart(10, ' '))
+      ds.push(bs[2].rTime.padStart(8, ' '))
+      ds.push(bs[2].rRate.padStart(10, ' '))
+      ds.push(bs[3].s)
+      ds.push(bs[3].wTime.padStart(8, ' '))
+      ds.push(bs[3].wRate.padStart(10, ' '))
+      ds.push(bs[3].rTime.padStart(8, ' '))
+      ds.push(bs[3].rRate.padStart(10, ' '))
+      ds.push(bs[4].s)
+      ds.push(bs[4].dTime.padStart(8, ' '))
+      ds.push(bs[4].dRate.padStart(10, ' '))
+      // console.log(ds.join(' | '))
+      data.push(ds)
     })
 
-    const loop = () => {
-      let ifaceObj = os.networkInterfaces()
-      let ifaces = []
-      for (var key in ifaceObj) {
-        if (key.startsWith('enx')) {
-          let ipv4 = ifaceObj[key].find(x => x.family === 'IPv4')
-          if (ipv4) {
-            ifaces.push(Object.assign({}, ipv4, { name: key }))
-          } 
-        }
-      }
-
-      ifaces.forEach(x => {
-        if (!Soldiers.has(x.mac)){
-          let s = new Soldier(x, ssid, password)
-          s.on('tick', () => {
-            // process.stdout.write('\033[2J\033[;H')
-            // console.log('设备              | 压力测试 | EMMC测试                                             | SATA测试                                             | WiFi测试') 
-            let hs = []
-            hs.push(' '.padEnd(17, ' '))
-            hs.push('状态'.padEnd(6, ' '))
-            hs.push('状态')
-            hs.push('写入次数'.padStart(8 - 4, ' '))
-            hs.push('速度'.padStart(10 - 2, ' '))
-            hs.push('读取次数'.padStart(8 - 4, ' '))
-            hs.push('速度'.padStart(10 - 2, ' '))
-            hs.push('状态')
-            hs.push('写入次数'.padStart(8 - 4, ' '))
-            hs.push('速度'.padStart(10 - 2, ' '))
-            hs.push('读取次数'.padStart(8 - 4, ' '))
-            hs.push('速度'.padStart(10 - 2, ' '))
-            hs.push('状态')
-            hs.push('下载次数'.padStart(8 - 4, ' '))
-            hs.push('速度'.padStart(10 - 2, ' '))
-            // console.log(hs.join(' | '))
-            // console.log(''.padEnd(169, '-'))
-/*
-            Array.from(Soldiers).forEach(kv => {
-              let bs = kv[1].brief() 
-              let ds = []
-              ds.push(bs[0].padEnd(17, ' '))
-              ds.push(bs[1].s.padEnd(6, ' '))
-              ds.push(bs[2].s)
-              ds.push(bs[2].wTime.padStart(8, ' '))
-              ds.push(bs[2].wRate.padStart(10, ' '))
-              ds.push(bs[2].rTime.padStart(8, ' '))
-              ds.push(bs[2].rRate.padStart(10, ' '))
-              ds.push(bs[3].s)
-              ds.push(bs[3].wTime.padStart(8, ' '))
-              ds.push(bs[3].wRate.padStart(10, ' '))
-              ds.push(bs[3].rTime.padStart(8, ' '))
-              ds.push(bs[3].rRate.padStart(10, ' '))
-              ds.push(bs[4].s)
-              ds.push(bs[4].dTime.padStart(8, ' '))
-              ds.push(bs[4].dRate.padStart(10, ' '))
-              // console.log(ds.join(' | '))
-            })
-*/
-
-          })
-        }
-      })
-    }
-
-    loop()
-    setInterval(loop, 3000)
+    blist.setData(data)
   })
+  Soldiers.set(iface.mac, soldier)
+  blog.log(`发现新设备：${iface.mac}`)
+})
+
+ifacemon.on('remove', iface => {
+  let soldier = Soldiers.get(iface.mac)
+  if (soldier) {
+    soldier.destroy()
+    Soldiers.delete(iface.mac)
+  }
 })
 
 
